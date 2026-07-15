916e67debbde505926467c3f8703ca3b5afdbc9e
Obetta Augustine <hebrusgraphics@gmail.com>
2026-07-15 23:13:36 +0100

    email bug solving

--- src/services/EmailService.ts
+++ src/services/EmailService.ts
@@ -31,7 +31,9 @@ interface SendSaleskeeperInviteOptions {
 
 function createTransport(): Transporter {
   return nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
